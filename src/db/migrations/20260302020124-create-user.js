4fe669cd7633e1741805db400c8d7d3deaec310e
Haikal <haikalhakam422@gmail.com>
2026-03-02 09:13:46 +0700

    membuat database dan tabel user

---
+++ src/db/migrations/20260302020124-create-user.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nama: {
+        type: Sequelize.STRING(100),
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      profile: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user");
+  },
+};
